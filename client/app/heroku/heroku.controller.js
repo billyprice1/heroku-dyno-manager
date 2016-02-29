b296391986d3b246db0a5497c46a188be01406dd
Manoj Nama <manoj.nama@outlook.com>
2016-02-29 11:56:10 +0530

    Using the APi to get dyno info

--- client/app/heroku/heroku.controller.js
+++ client/app/heroku/heroku.controller.js
@@ -12,40 +12,11 @@ angular.module('herokuDynoManagerApp')
       var self = this;
 
       self.appName = $stateParams.appId;
-      //HerokuApi.dynos({
-      //   appId: self.appName
-      //}, function (resp) {
-      //   self.dynos = resp;
-      //});
-
-      self.dynos = [
-         {
-            "attach_url": null,
-            "command": "node --optimize_for_size --max_old_space_size=960 --expose-gc dist/server/app.js",
-            "created_at": "2016-02-29T04:27:43Z",
-            "id": "0b1c35df-fbdd-495f-bded-5053ce16821d",
-            "name": "web.1",
-            "app": {"id": "80332a31-03dd-4eec-b517-4517061cdb93", "name": "prod-kokaihop"},
-            "release": {"id": "5a3c3d81-774c-4f5f-bddc-8cd93133c2bb", "version": 386},
-            "size": "Standard-2X",
-            "state": "up",
-            "type": "web",
-            "updated_at": "2016-02-29T04:27:43Z"
-         },
-         {
-            "attach_url": null,
-            "command": "node --optimize_for_size --max_old_space_size=460 dist/server/worker.js",
-            "created_at": "2016-02-28T21:08:39Z",
-            "id": "6fbed194-0af4-4eeb-96e1-a3783db2beae",
-            "name": "worker.1",
-            "app": {"id": "80332a31-03dd-4eec-b517-4517061cdb93", "name": "prod-kokaihop"},
-            "release": {"id": "5a3c3d81-774c-4f5f-bddc-8cd93133c2bb", "version": 386},
-            "size": "Standard-1X",
-            "state": "up",
-            "type": "worker",
-            "updated_at": "2016-02-28T21:08:39Z"
-         }
-      ];
+      HerokuApi.dynos({
+         appId: self.appName
+      }, function (resp) {
+         self.dynos = resp;
+      });
 
       self.restart = function (dynoId, idx) {
          self.dynos[idx].restartRequestSent = true;
