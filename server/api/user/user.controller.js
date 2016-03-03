c87bcf78c49e05a91f86fae16faf529960e2d023
Manoj Nama <manoj.nama@outlook.com>
2016-03-03 22:07:12 +0530

    Updated login page design, added user projection for data sent to the client and attached to request obj

--- server/api/user/user.controller.js
+++ server/api/user/user.controller.js
@@ -29,7 +29,7 @@ exports.me = function(req, res, next) {
    var userId = req.user._id;
    User.findOne({
       _id: userId
-   }, '-salt -hashedPassword', function(err, user) { // don't ever give out the password or salt
+   }, '-salt -hashedPassword -accessToken -heroku', function(err, user) { // don't ever give out the password or salt
       if (err) return next(err);
       if (!user) return res.status(401).send('Unauthorized');
       res.json(user);
