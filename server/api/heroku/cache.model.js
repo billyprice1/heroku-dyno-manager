97e808d05cde247e40f99e97f8ee26e46e159361
Manoj Nama <manoj.nama@outlook.com>
2016-03-13 11:20:31 +0530

    Updated the cache model to use cache enums

--- server/api/heroku/cache.model.js
+++ server/api/heroku/cache.model.js
@@ -1,11 +1,12 @@
 'use strict';
 
 var mongoose = require('mongoose'),
+   CacheEnum = require("../../enum/cache.enum"),
    Schema = mongoose.Schema;
 
 var CacheSchema = new Schema({
    createdAt: {type: Date, expires: '30m', default: Date},
-   type: {type: String},
+   type: {type: String, enum: CacheEnum.list, default: CacheEnum.types.APPS},
    userToken: String,
    appId: String,
    data: {}
