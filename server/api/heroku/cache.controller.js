00b3c751fbca0c89257b95fbd9265690a3e77c6f
Manoj Nama <manoj.nama@outlook.com>
2016-03-13 12:06:46 +0530

    Added caching for config vars API

--- server/api/heroku/cache.controller.js
+++ server/api/heroku/cache.controller.js
@@ -44,3 +44,24 @@ exports.collaborators = function (req, res, next) {
       next();
    }
 };
+
+exports.config = function (req, res, next) {
+   var appId = req.params.appId;
+   if (appId) {
+      Cache
+         .findOne({appId: appId, type: CacheEnum.types.CONFIG})
+         .lean()
+         .exec(function (err, doc) {
+            if(err) {
+               console.log("Cache error:", err);
+               return next();
+            } else if(doc) {
+               return res.json(doc.data.value);
+            } else {
+               next();
+            }
+         });
+   } else {
+      next();
+   }
+};
--- server/api/heroku/index.js
+++ server/api/heroku/index.js
@@ -11,11 +11,11 @@ router.post('/', auth.isAuthenticated(), CacheController.apps, controller.apps);
 router.post('/dynos/:appId', auth.isAuthenticated(), controller.dynos);
 router.post('/restart/:appId/:dynoId', auth.isAuthenticated(), controller.restart);
 
-router.post("/config/list/:appId", auth.isAuthenticated(), controller.getConfig);
+router.post("/config/list/:appId", auth.isAuthenticated(), CacheController.config, controller.getConfig);
 router.post("/config/create/:appId", auth.isAuthenticated(), controller.setConfig);
 router.post("/config/remove/:appId", auth.isAuthenticated(), controller.removeConfig);
 
-router.post("/collaborators/list/:appId", auth.isAuthenticated(), controller.listCollaborators);
+router.post("/collaborators/list/:appId", auth.isAuthenticated(), CacheController.collaborators, controller.listCollaborators);
 router.post("/collaborators/create/:appId", auth.isAuthenticated(), controller.createCollaborator);
 router.post("/collaborators/show/:appId/:collaboratorId", auth.isAuthenticated(), controller.getCollaborator);
 router.post("/collaborators/remove/:appId/:collaboratorId", auth.isAuthenticated(), controller.removeCollaborator);
