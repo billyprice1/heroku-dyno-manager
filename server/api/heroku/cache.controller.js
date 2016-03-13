583d0b311cfef4a22b005c5e3908cdee244d8894
Manoj Nama <manoj.nama@outlook.com>
2016-03-13 11:23:22 +0530

    Adding cache controller with cache checking middlewares

---
+++ server/api/heroku/cache.controller.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var Cache = require("./cache.model"),
+   CacheEnum = require("../../enum/cache.enum");
+
+exports.apps = function (req, res, next) {
+   var user = req.user;
+   if (user.accessToken) {
+      Cache
+         .findOne({userToken: user.accessToken, type: CacheEnum.types.APPS})
+         .lean()
+         .exec(function (err, doc) {
+            if(err) {
+               console.log("Cache error:", err);
+               return next();
+            }
+            return res.json(doc.data.value);
+         });
+   } else {
+      next();
+   }
+};
