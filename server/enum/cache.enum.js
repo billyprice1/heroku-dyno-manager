80eff91e2451ea30935ca9cd1d29e7bf29d81406
Manoj Nama <manoj.nama@outlook.com>
2016-03-13 11:19:08 +0530

    Adding enums to differentiate cache data types

---
+++ server/enum/cache.enum.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var types = {
+   APPS: "APPS",
+   COLLABORATORS: "COLLABORATORS",
+   CONFIG: "CONFIG"
+};
+
+exports.types = types;
+
+exports.list = [types.APPS, types.COLLABORATORS, types.CONFIG];
