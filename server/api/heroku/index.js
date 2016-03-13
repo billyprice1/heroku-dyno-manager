99298dfffa2085356d769286dcedcd49adbdd2a4
Manoj Nama <manoj.nama@outlook.com>
2016-03-13 11:23:43 +0530

    Using cache middleware for apps API

--- server/api/heroku/index.js
+++ server/api/heroku/index.js
@@ -2,11 +2,12 @@
 
 var express = require('express'),
    auth = require("../../auth/auth.service"),
+   CacheController = require("./cache.model"),
    controller = require('./heroku.controller');
 
 var router = express.Router();
 
-router.post('/', auth.isAuthenticated(), controller.apps);
+router.post('/', auth.isAuthenticated(), CacheController.apps, controller.apps);
 router.post('/dynos/:appId', auth.isAuthenticated(), controller.dynos);
 router.post('/restart/:appId/:dynoId', auth.isAuthenticated(), controller.restart);
 
